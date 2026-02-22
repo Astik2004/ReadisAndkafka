9e5c7c6d729b2f44b80ed29e8aab3b6a4df1d77f
Astik Yadav <astikyadav4@gmail.com>
2026-02-22 10:23:29 +0530

    Project setup for Redis and Kafka

---
+++ redistut/src/test/java/com/astik/reddis/redistut/RedistutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astik.reddis.redistut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedistutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
